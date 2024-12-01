f5a22130a3f0963a0619ae626df9b396742a0780
efraimff <emfs6@aluno.ifal.edu.br>
2024-12-01 11:32:41 -0300

    feat: update message list layout
    - Implemented layout for message list with user images and badge indicator.
    - Added custom styling for text (name, message, date).
    - Integrated a badge component to display unread message count.

--- app/src/main/java/com/example/jetpack_layout/MainActivity.kt
+++ app/src/main/java/com/example/jetpack_layout/MainActivity.kt
@@ -3,27 +3,37 @@ package com.example.jetpack_layout
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment.Companion.CenterVertically
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.jetpack_layout.ui.theme.Jetpack_LayoutTheme
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import coil3.compose.AsyncImage
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContent {
-            Jetpack_LayoutTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+        setContent{
+            Scaffold { innerPadding ->
+                Surface (
+                    modifier = Modifier.padding(innerPadding)) {
+                    ListViewComposable()
                 }
             }
         }
@@ -31,17 +41,101 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun ListViewComposable(modifier: Modifier = Modifier) {
+    Column {
+        ListItemComposable(
+            modifier = Modifier,
+            name = "John Doe",
+            date = "Today",
+            message = "Hello, how are you?",
+            number = 8
+        )
+    }
 }
 
-@Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    Jetpack_LayoutTheme {
-        Greeting("Android")
+fun ListItemComposable(modifier: Modifier = Modifier, name: String, date: String, message: String, number: Int) {
+    Row (
+        Modifier.
+            padding(horizontal = 12.dp, vertical = 8.dp)
+            .fillMaxWidth(),
+        verticalAlignment = CenterVertically
+    ) {
+        AsyncImage(
+            model = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTdQLwDqDwd2JfzifvfBTFT8I7iKFFevcedYg&s",
+            contentDescription = "",
+            contentScale = ContentScale.Crop,
+            modifier = Modifier
+                .padding(top = 14.dp)
+                .size(50.dp)
+                .clip(CircleShape)
+                .background(Color.Gray)
+        )
+
+        Column (
+            Modifier
+                .padding(start = 16.dp, top = 18.dp)
+                .fillMaxWidth(0.85f)
+        ) {
+
+            Text(
+               text = name,
+               fontSize = 16.sp,
+               fontWeight = FontWeight.Bold,
+           )
+
+            Text(
+                text = message,
+                color = Color.Gray,
+                modifier = Modifier.padding(top = 6.dp)
+            )
+        }
+
+        Column (
+            Modifier
+                .padding(top = 20.dp),
+                horizontalAlignment = androidx.compose.ui.Alignment.End
+        ) {
+            Text(
+                text = date,
+                fontSize = 12.sp,
+                color = Color.Gray,
+            )
+
+            BadgeComposable(number)
+        }
+    }
+}
+
+@Composable
+fun BadgeComposable(number: Int) {
+    if (number > 0) {
+        Surface(
+            color = Color.Green,
+            contentColor = Color.White,
+            shape = CircleShape,
+            modifier = Modifier.padding(4.dp)
+        ) {
+            Text(
+                text = number.toString(),
+                fontSize = 12.sp,
+                modifier = Modifier.padding(horizontal = 4.dp)
+            )
+        }
+    }
+}
+
+@Preview(
+    showBackground = true,
+    showSystemUi = true,
+    device = Devices.PIXEL_7A
+)
+
+@Composable
+fun ListViewComposablePreview() {
+    Scaffold { innerPadding ->
+        Surface (modifier = Modifier.padding(innerPadding)) {
+            ListViewComposable()
+        }
     }
 }
